08a6ffe7c7d6c659d932aea9b44ba352b7c9af1a
bo-rai-cho <email>
2018-05-10 15:10:04 -0400

    Dockerimage changed

--- src/main/java/com/mastercard/samples/model/Card.java
+++ src/main/java/com/mastercard/samples/model/Card.java
@@ -14,9 +14,9 @@ import lombok.Data;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public final class Card {
 
-    private String ooowner;
+    private String owner;
     private Long cardNumber;
-    private Boolean active;
+    private Boolean valid;
 
     public String toJsonString() {
 
